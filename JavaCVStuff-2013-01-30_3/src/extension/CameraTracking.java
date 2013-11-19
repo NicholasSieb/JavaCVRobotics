f1eeeba3106e9b283b072e910745581550b9f444
AbstractVoid <nasieb@wisc.edu>
2013-11-19 17:48:10 -0600

    Initial

---
+++ JavaCVStuff-2013-01-30_3/src/extension/CameraTracking.java
@@ -0,0 +1,12 @@
+package extension;
+
+import edu.wpi.first.smartdashboard.gui.StaticWidget;
+import edu.wpi.first.smartdashboard.properties.Property;
+
+public class CameraTracking{// extends StaticWidget {
+    //@Override
+    //void propertyChanged(Property) {
+        
+    //}
+
+}
